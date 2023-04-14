746b303cc17970df53fd3a1674c5958ca0987ae5
Quado2 <engchikwas404@gmail.com>
2023-04-14 09:18:25 +0100

    removed devise integrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
  
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-
-
   get '/current_user', to: 'current_user#index'
 
 end
