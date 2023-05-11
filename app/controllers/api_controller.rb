a342fbf093b3bf8a03ee6482f02e3a368d9696bf
Quado2 <engchikwas404@gmail.com>
2023-05-11 23:26:14 +0100

    remove .env from tracked files

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ActionController::API
   include ApplicationHelper
+  include Constants
   include Serializable
   rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
   rescue_from ActiveRecord::RecordInvalid, with: :unprocessable
